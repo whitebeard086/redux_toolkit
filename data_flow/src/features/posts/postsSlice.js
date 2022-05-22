897a53f64da994ca45c3d8ebcb570370fc75fcd3
unknown <whitebeard086@gmail.com>
2022-05-22 02:39:42 +0100

    dynamic initial state

--- data_flow/src/features/posts/postsSlice.js
+++ data_flow/src/features/posts/postsSlice.js
@@ -1,34 +1,11 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 import { sub } from "date-fns";
 
-const initialState = [
-  {
-    id: "1",
-    title: "Learning Redux Toolkit",
-    content: "I've heard good things.",
-    date: sub(new Date(), { minutes: 10 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-  {
-    id: "2",
-    title: "Slices...those things!",
-    content: "The more I say slice, the more I want Pizza!",
-    date: sub(new Date(), { minutes: 5 }).toISOString(),
-    reactions: {
-      thumbsUp: 0,
-      wow: 0,
-      heart: 0,
-      rocket: 0,
-      coffee: 0,
-    },
-  },
-];
+const initialState = {
+  posts: [],
+  status: "idle", // "idle" | "loading" | "succeeded" | "failed"
+  error: null
+}
 
 const postsSlice = createSlice({
   name: "posts",
@@ -36,7 +13,7 @@ const postsSlice = createSlice({
   reducers: {
     postAdded: {
       reducer(state, action) {
-        state.push(action.payload);
+        state.posts.push(action.payload);
       },
       prepare(title, content, userId) {
         return {
@@ -59,7 +36,7 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload;
-      const existingPost = state.find(post => post.id === postId);
+      const existingPost = state.posts.find(post => post.id === postId);
 
       if (existingPost) {
         existingPost.reactions[reaction]++;
@@ -68,7 +45,7 @@ const postsSlice = createSlice({
   },
 });
 
-export const selectAllPosts = state => state.posts;
+export const selectAllPosts = state => state.posts.posts;
 
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
