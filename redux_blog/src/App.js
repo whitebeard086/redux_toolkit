ecfcd23d6b3e42c137369775bc55eb8ff14280aa
unknown <whitebeard086@gmail.com>
2022-05-27 08:50:03 +0100

    memoize selector to optimize perfomance

--- redux_blog/src/App.js
+++ redux_blog/src/App.js
@@ -1,15 +1,18 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import PostsList from "./features/posts/PostsList";
 import AddPostForm from "./features/posts/AddPostForm";
 import SinglePostPage from "./features/posts/SinglePostsPage";
 import EditPostForm from "./features/posts/EditPostForm";
+import UsersList from "./features/users/UsersList";
+import UserPage from "./features/users/UserPage";
 import Layout from "./components/Layout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+
         <Route index element={<PostsList />} />
 
         <Route path="post">
@@ -17,7 +20,15 @@ function App() {
           <Route path=":postId" element={<SinglePostPage />} />
           <Route path="edit/:postId" element={<EditPostForm />} />
         </Route>
+
+        <Route path="user">
+          <Route index element={<UsersList />} />
+          <Route path=":userId" element={<UserPage />} />
+        </Route>
       </Route>
+
+      <Route path="*" element={<Navigate to="/" replace />} />
+
     </Routes>
   );
 }
