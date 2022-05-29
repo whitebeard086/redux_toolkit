49e5a5e77837d3ac81880897fd5f20b911f76b9b
unknown <whitebeard086@gmail.com>
2022-05-29 04:41:21 +0100

    normalized state for performance optimization

--- redux_blog/src/features/posts/PostsExcerpt.jsx
+++ redux_blog/src/features/posts/PostsExcerpt.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
+import { selectPostById } from "./postsSlice";
 
-const PostsExcerpt = ({ post }) => {
+const PostsExcerpt = ({ postId }) => {
+    const post = useSelector((state) => selectPostById(state, postId))
+    
     return (
         <article>
             <h2>{post.title}</h2>
